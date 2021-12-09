5520c3a10095b2688cee70d9ce1e216b93ce47b4
Fracs97 <fracassobruno@gmail.com>
2021-12-09 16:49:53 -0300

    Fatores clientes consertados

--- prep-e-modelagem.R
+++ prep-e-modelagem.R
@@ -2,59 +2,57 @@ library(dplyr)
 library(data.table)
 library(caret)
 library(rpart)
-library(kernlab)
-library(randomForest)
+library(tidyr)
 
 set.seed(100)
 
+#Os códigos dos clientes são por ordem de chegada, e não tem valor algum por si só, essa variável só possui alguma
+#utilidade quando substituída pelos nomes reais dos clientes, que estão no arquivo cliente_tabla.csv
+clientes = fread('cliente_tabla.csv')
+
 dados = fread('train_enxuto.csv')
+
+#Removendo as duplicatas para ser possível realizar o join (limitação de ram)
+dados = distinct(dados)
+
+dados = dados %>% left_join(clientes)
 
 str(dados)
 
-#Há fatores demais (880 mil)
+#As variáveis preditoras são ID's, e por isso são fatores
+dados = dados %>% mutate_at(c(1,2,3,4,5,7),as.factor)
+
+#A coluna Cliente_ID não é mais necessária
 dados$Cliente_ID = NULL
 
-#As variáveis preditoras são ID's, e por isso são fatores
-dados = dados %>% mutate_at(c(1:4),as.factor)
-
-#Aqui há um grande problema, pois uma mesma combinação de variáveis de entrada gera
-#saídas diferentes
-sum(duplicated(dados[,c(1:5)]))
-#Isso é racionalmente aceitável, pois um mesmo cliente pode fazer pedidos de dimensões 
-#diferentes usando as mesmas opções de compra, pois a demanda não é constante.
-#O problema é que, na matemática, um x não pode gerar dois y diferentes
-
-#Para resolver isso, vou estimar que uma dada condição de compra sempre gera uma demanda
-#constante
-dados = distinct_at(dados,c(1:4),.keep_all=T)
-
+#Como haviam ID's diferentes para um mesmo cliente, é preciso remover as novas duplicatas
 dados = distinct(dados)
+
 #Não há nenhum nulo
 sapply(dados,function(x)sum(is.na(x)))
 
+#Deixando 
 #MODELAGEM
-
 #Como a base de dados é muito grande, para ser possível fazer o treinamento eu criei um 
-#modelo para cada produto através de um subset dos dados
-sub_prod = function(produto){
-  dados_prod = subset(dados, Producto_ID==produto,-4)
+#modelo para cada rota através de um subset dos dados
+sub_rota = function(rota){
+  dados_rota = subset(dados, Ruta_SAK==rota,-3)
 }
 
-#LISTA DE FREQUENCIAS DE CADA PRODUTO
-data.frame(table(dados$Producto_ID)) %>% arrange(Freq) %>% View()
-dados_prod = sub_prod('41843')
-dados_prod = sub_prod('5337')
+#LISTA DE FREQUENCIAS DE CADA ROTA
+data.frame(table(dados$Ruta_SAK)) %>% arrange(Freq) %>% View()
+dados_rota = sub_rota('2106')
+
 #Separando em treino e teste
-tt = createDataPartition(dados_prod$Demanda_uni_equil,p=0.7,list = F)
-treino = dados_prod[tt,]
-teste = dados_prod[-tt,]
-#Para poder usar essa variável na função de avaliação (tornando ela global)
+tt = createDataPartition(dados_rota$Demanda_uni_equil,p=0.7,list = F)
+treino = dados_rota[tt,]
+teste = dados_rota[-tt,]
 
 sprintf('Treino: %d, teste: %d',dim(treino)[1],dim(teste)[1])
 
 #Função que automatiza a avaliação do algoritmo
 avalia=function(modelo){
-  previsoes = predict(modelo,teste[,1:(ncol(teste)-1)])
+  previsoes = predict(modelo,teste[,-4])
   compara = data.frame(real = teste$Demanda_uni_equil,previsto=previsoes)
   cat(sprintf('#MAE: %g',MAE(previsoes,teste$Demanda_uni_equil)))
   cat('\n')
@@ -67,17 +65,27 @@ avalia=function(modelo){
 #RPART
 modelo_rpart = rpart(Demanda_uni_equil~.,data=treino)
 df_rpart = avalia(modelo_rpart)
+rmsle(df_rpart$previsto,df_rpart$real)
+#Separando por produto
 #MAE: 7.71428
 #Resíduos: 476.56
 #R²: 0.185365
 
+#Separando por rota
+#MAE: 13.4776
+#Resíduos: 1082.91
+#R²: 0.350835
+
+#MAE: 3.27299
+#Resíduos: -217.24
+#R²: 0.359853
+
 #CUBIST (Regressão linear)
-modelo_cub = cubist(treino[,1:3],treino$Demanda_uni_equil,committees = 10)
+modelo_cub = cubist(treino[,1:3],treino$Demanda_uni_equil,committees = 3)
 df_cub = avalia(modelo_cub)
-#MAE: 21.7591
-#Resíduos: 9030.1
-#R²: 0.208888
-#Usando neighbors=9
+#MAE: 11.2576
+#Resíduos: 89179.2
+#R²: 0.297063
 
 #SVM
 modelo_svm = ksvm(Demanda_uni_equil~.,data=treino,kernel='rbfdot')
@@ -86,6 +94,3 @@ df_svm = avalia(modelo_svm)
 #MAE: 0.970225
 #Resíduos: 1937.87
 #R²: 0.129979
-
-modelo_svm2 = svm(Demanda_uni_equil~.,data=treino,kernel='linear')
-df_svm2 = avalia(modelo_svm2)
